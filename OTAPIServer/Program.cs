991ea5ced4e4b83f7a9ed9270cb0e2e5e29dd61c
Axeel <axeel@qq.com>
2021-08-18 19:29:02 +0800

    Add a demo project

---
+++ OTAPIServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OTAPIServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terraria.WindowsLaunch.Main(args);
+        }
+    }
+}
