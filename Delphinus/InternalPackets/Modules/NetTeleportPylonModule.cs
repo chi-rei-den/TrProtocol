8bf5d557972fe724e85611d8d04e9ebb34ba4fcb
Axeel <axeel@qq.com>
2021-08-18 09:12:53 +0800

    Fix NetTeleportPylonModule

--- Delphinus/InternalPackets/Modules/NetTeleportPylonModule.cs
+++ Delphinus/InternalPackets/Modules/NetTeleportPylonModule.cs
@@ -5,8 +5,11 @@ namespace Delphinus.InternalPackets.Modules
     internal class NetTeleportPylonModule : IPacket
     {
         public SubPacketType Type { get; set; }
+        [Condition("(byte){{packet}}.Type == 2")]
         public short PosX { get; set; }
+        [Condition("(byte){{packet}}.Type == 2")]
         public short PosY { get; set; }
+        [Condition("(byte){{packet}}.Type == 2")]
         public TeleportPylonType PylonType { get; set; }
     }
 }
